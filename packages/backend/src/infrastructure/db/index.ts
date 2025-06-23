306bb7ce85da335ffb1ad4d9baa510ee1a7beaaf
betich <betich365@gmail.com>
2025-06-23 23:05:50 +0700

    feat: set up sqlite database and drizzle config

---
+++ packages/backend/src/infrastructure/db/index.ts
@@ -0,0 +1,7 @@
+import { drizzle } from "drizzle-orm/better-sqlite3";
+import * as schema from "./schema";
+import { env } from "@/env";
+
+export const db = drizzle(env.DATABASE_URL, { schema });
+
+export type DrizzleClient = typeof db;
