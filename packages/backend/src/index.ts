44fa9224a9e5598333f88ccac7c620c3a988dc96
betich <betich365@gmail.com>
2025-06-30 21:59:12 +0700

    feat: api documentation

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,7 +1,11 @@
+import { Scalar } from "@scalar/hono-api-reference";
+
 import { Hono } from "hono";
+import { openAPISpecs } from "hono-openapi";
+
+import { agendaRouter } from "./modules/agenda";
 import { eventRouter } from "./modules/event";
 import { userRouter } from "./modules/user";
-import { agendaRouter } from "./modules/agenda";
 
 const app = new Hono()
   .basePath("/api")
@@ -11,5 +15,21 @@ const app = new Hono()
   .route("/event", eventRouter)
   .route("/user", userRouter)
   .route("/agenda", agendaRouter);
+
+app.get(
+  "/openapi",
+  openAPISpecs(app, {
+    documentation: {
+      info: {
+        title: "Eventer API",
+        version: "1.0.0",
+        description: "Hey!",
+      },
+      servers: [{ url: "http://localhost:3000", description: "Local Server" }],
+    },
+  })
+);
+
+app.get("/scalar", Scalar({ url: "/api/openapi" }));
 
 export { app };
