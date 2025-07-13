c85a860b664b3ffeb0c4096876d3565ccbb6c8fb
betich <betich365@gmail.com>
2025-07-14 01:10:48 +0700

    fix: fix tests

--- apps/backend/vitest.config.ts
+++ apps/backend/vitest.config.ts
@@ -4,14 +4,8 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
 	test: {
 		globals: true,
-		environment: "bun",
+		environment: "node",
 		include: ["src/**/*.test.ts"],
-		pool: "forks",
-		poolOptions: {
-			forks: {
-				singleFork: true,
-			},
-		},
 		env: {
 			...config({ path: ".env.test" }).parsed,
 		},
