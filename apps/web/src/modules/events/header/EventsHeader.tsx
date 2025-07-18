29ce0d4af881ca4b882d9bc9817c16dfd6d6ec7b
betich <betich365@gmail.com>
2025-07-19 03:27:47 +0700

    feat: add a welcoming message to header bar

--- apps/web/src/modules/events/header/EventsHeader.tsx
+++ apps/web/src/modules/events/header/EventsHeader.tsx
@@ -1,13 +1,17 @@
 "use client";
 import { Button } from "@/components/atoms/button";
 import { useLogOutMutation } from "@/hooks/mutations/use-log-out";
+import { useSession } from "@/hooks/use-session";
 
 export function EventsHeader() {
 	const { mutate: logOut } = useLogOutMutation();
+	const { user, isLoading, error } = useSession();
 
 	return (
-		<header className="bg-gray-800 text-white flex justify-between p-4">
-			<h1 className="text-xl">Dashboard</h1>
+		<header className="bg-gray-800 text-white flex items-center justify-between px-2 py-4">
+			<h1 className="text-base">
+				{isLoading ? "Loading ..." : error ? "error" : `Hi, ${user?.name}!`}
+			</h1>
 			<Button type="button" variant="destructive" onClick={() => logOut()}>
 				Logout
 			</Button>
