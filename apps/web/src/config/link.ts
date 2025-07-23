8c6bfd970ffc523520b467cf302a257d52352c92
Thai Pangsakulyanont <dtinth@spacet.me>
2025-07-23 07:04:49 +0000

    Authgarten integration

--- apps/web/src/config/link.ts
+++ apps/web/src/config/link.ts
@@ -2,4 +2,5 @@ import { env } from "@/env";
 
 const REDIRDCT_URI = encodeURIComponent(`${env.NEXT_PUBLIC_APP_URL}/auth/callback`);
 
-export const SIGN_IN_LINK = `https://qtrkroiyvtnwdpjscyvp.supabase.co/auth/v1/authorize?provider=google&redirect_to=${REDIRDCT_URI}&scopes=email%20profile`;
+// http://localhost:54321/auth/v1/authorize?provider=keycloak&redirect_to=http://localhost:3000/auth/callback
+export const SIGN_IN_LINK = `http://localhost:54321/auth/v1/authorize?provider=keycloak&redirect_to=http://localhost:3000/auth/callback`; // `https://qtrkroiyvtnwdpjscyvp.supabase.co/auth/v1/authorize?provider=google&redirect_to=${REDIRDCT_URI}&scopes=email%20profile`;
